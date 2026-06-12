5fcc755380eead84862d6bb3a35a1aabdd4089e3
IgorDanielLustosa <igordaniel1123@gmail.com>
2026-06-11 22:31:53 -0300

    Cliente java

---
+++ src/main/java/com/barbearia/entity/Cliente.java
@@ -0,0 +1,8 @@
+package com.barbearia.entity;
+import jakarta.persistence;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import lombok.EqualsAndHashCode;
+import java.time.LocalDAte;
